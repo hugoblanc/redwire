3394bf461d0e9c42595031769613f9410aa0e232
hugo <MrWhite>
2019-10-16 10:36:24 +0200

    Add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListArticlesComponent } from './list-articles/list-articles.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/list-articles' },
+  { path: 'list-articles', component: ListArticlesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
